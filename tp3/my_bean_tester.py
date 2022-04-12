bb412c43312cb2fa6b477a7aabf36496a8153c5e
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2022-04-12 16:29:06 -0400

    trash model tp3

--- tp3/my_bean_tester.py
+++ tp3/my_bean_tester.py
@@ -6,15 +6,48 @@ Authors:
 <<<<< NOM COMPLET #2 - MATRICULE #2 >>>>>
 """
 
-BEANS = ['SIRA','HOROZ','DERMASON','BARBUNYA','CALI','BOMBAY','SEKER']
+BEANS = ["SIRA", "HOROZ", "DERMASON", "BARBUNYA", "CALI", "BOMBAY", "SEKER"]
+BEANS_INDEXES = {BEANS[i]: i for i in range(len(BEANS))}
 
 from bean_testers import BeanTester
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 
 class MyBeanTester(BeanTester):
     def __init__(self):
         # TODO: initialiser votre modèle ici:
-        pass
+        self.model = keras.models.Sequential()
+        self.model.add(keras.layers.Flatten())
+        self.model.add(keras.layers.Dense(12, activation=tf.nn.sigmoid))
+        self.model.add(keras.layers.Dense(3, activation=tf.nn.sigmoid))
+        self.model.add(keras.layers.Dense(len(BEANS), activation=tf.nn.sigmoid))
+
+        self.model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=0.3),
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+
+    def min_max_scaler(self, features):
+        """
+        Function: scales values betweeb 0 and 1.
+
+        Parameters: dataset as an NumPy array.
+
+        Returns: scaled NumPy array in float32 dtype.
+        """
+        max_n = np.max(features)
+        min_n = np.min(features)
+        features_scaled = np.array([(x - min_n) / (max_n - min_n) for x in features])
+        return features_scaled.astype("float32")
+
+    def preprocess_data(self, x_data):
+        x_train = [[float(feature) for feature in features[1:]] for features in x_data]
+        x_train = self.min_max_scaler(x_train)
+        return x_train
+
 
     def train(self, X_train, y_train):
         """
@@ -29,7 +62,16 @@ class MyBeanTester(BeanTester):
                 the second column is the example label.
         """
         # TODO: entrainer un modèle sur X_train & y_train
-        raise NotImplementedError()
+
+        # mnist = keras.datasets.mnist
+        # ((x_train, y_train), (x_test, y_test)) = mnist.load_data()
+        # print(x_train)
+
+        x_train = self.preprocess_data(X_train)
+        y_train = [BEANS_INDEXES[bean[1]] for bean in y_train]
+
+        # print(y_train)
+        self.model.fit(x_train, np.array(y_train), epochs=3000)
 
     def predict(self, X_data):
         """
@@ -48,4 +90,8 @@ class MyBeanTester(BeanTester):
         :return: a 2D list of predictions with 2 columns: ID and prediction
         """
         # TODO: make predictions on X_data and return them
-        raise NotImplementedError()
+        x_data = self.preprocess_data(X_data)
+        predictions = self.model.predict(x_data)
+        predictions = [[i + 1, BEANS[np.argmax(prediction)]] for i, prediction in enumerate(predictions)]
+
+        return predictions
